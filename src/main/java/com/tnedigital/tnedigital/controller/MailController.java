b5caaf02b19af4330ca25c40861c1901f443e2a2
renato.caetano <renatocsare@gmail.com>
2018-07-31 15:48:13 -0300

    testes

--- src/main/java/com/tnedigital/tnedigital/controller/MailController.java
+++ src/main/java/com/tnedigital/tnedigital/controller/MailController.java
@@ -1,9 +1,17 @@
 package com.tnedigital.tnedigital.controller;
 
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.tnedigital.tnedigital.domain.Email;
 
 @RestController
 public class MailController {
@@ -14,6 +22,16 @@ public class MailController {
 		return "Olá mundo";
 	}
 	
+	@RequestMapping(value="/upload", method=RequestMethod.POST)
+	public void processUpload(@RequestParam MultipartFile file) throws IOException {
+	        System.out.println("Upload de arquivo");
+	        System.out.println(file.getName());
+	}
 	
-
+    @PostMapping("/enviar")
+    public void greetingSubmit(@ModelAttribute Email email) {
+        System.out.println(email.getTitulo());
+        System.out.println(email.getConteudo());
+    }
+	
 }
---
+++ src/main/java/com/tnedigital/tnedigital/domain/Email.java
@@ -0,0 +1,21 @@
+package com.tnedigital.tnedigital.domain;
+
+public class Email {
+	
+	private String titulo;
+	private String conteudo;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getConteudo() {
+		return conteudo;
+	}
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+	
+}
