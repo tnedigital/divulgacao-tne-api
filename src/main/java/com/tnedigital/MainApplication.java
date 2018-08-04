df53cbfff8e7337807295ad4eab5839c70bd26ee
Renato Caetano <Renato>
2018-08-04 00:22:50 -0300

    app engine conversion

--- src/main/java/com/tnedigital/MainApplication.java
+++ src/main/java/com/tnedigital/MainApplication.java
@@ -4,13 +4,14 @@ import java.util.concurrent.Executor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableAsync
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
